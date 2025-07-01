503e9ea5a1b9f2804a6a6b0afd884f16561a3e02
ren-web <rn.sgym@icloud.com>
2025-07-02 02:35:42 +0900

    2025年7月2日

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
 const hamburger = document.getElementById('hamburger');
 const header__list = document.querySelector('.header__list');
+const menuLinks = document.querySelectorAll('.header__list a');
+const logo = document.querySelector('.site__title');
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active');
     console.log('押されたよ');
     if(hamburger.classList.contains('active')) {
         header__list.classList.add('active');
+        logo.classList.add('active');
     } else {
         header__list.classList.remove('active');
+        logo.classList.remove('active');
     }
+})
+
+menuLinks.forEach(link => {
+    link.addEventListener('click',() => {
+        hamburger.classList.remove('active');
+        header__list.classList.remove('active');
+    })
+})
+
+logo.addEventListener('click',() => {
+    hamburger.classList.remove('active');
+    header__list.classList.remove('active');
 })
