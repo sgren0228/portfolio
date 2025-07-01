2960a4b418162846807cf556f97409d64bdba96d
ren-web <rn.sgym@icloud.com>
2025-07-02 03:00:38 +0900

    2025年7月2日

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const hamburger = document.getElementById('hamburger');
-const header__list = document.querySelector('.header__list');
+const headerList = document.querySelector('.header__list');
 const menuLinks = document.querySelectorAll('.header__list a');
 const logo = document.querySelector('.site__title');
 hamburger.addEventListener('click',() => {
     hamburger.classList.toggle('active');
     console.log('押されたよ');
     if(hamburger.classList.contains('active')) {
-        header__list.classList.add('active');
+        headerList.classList.add('active');
         logo.classList.add('active');
     } else {
-        header__list.classList.remove('active');
+        headerList.classList.remove('active');
         logo.classList.remove('active');
     }
 })
@@ -17,14 +17,14 @@ hamburger.addEventListener('click',() => {
 menuLinks.forEach(link => {
     link.addEventListener('click',() => {
         hamburger.classList.remove('active');
-        header__list.classList.remove('active');
+        headerList.classList.remove('active');
         logo.classList.remove('active');
     })
 })
 
 logo.addEventListener('click',() => {
     hamburger.classList.remove('active');
-    header__list.classList.remove('active');
+    headerList.classList.remove('active');
     logo.classList.remove('active');
 })
 
