2448290e93ffaba1577a532f54feca0dabaafd36
ren-web <rn.sgym@icloud.com>
2025-07-04 05:53:45 +0900

    2025年7月4日

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ logo.addEventListener('click',() => {
     headerList.classList.remove('active');
     logo.classList.remove('active');
 })
-
+// ヘッダー切り替え
 const header = document.getElementById('header');
 const fv = document.getElementById('fv');
 const fvHeight = fv.offsetHeight;
@@ -40,3 +40,51 @@ window.addEventListener('scroll',() => {
         header.classList.remove('scrolled');
     }
 })
+
+// モーダルウィンドウ
+const modal = document.querySelectorAll('modal');
+const mask = document.getElementById('mask');
+document.querySelectorAll('.works__item').forEach(item => {
+    item.addEventListener('click', function() {
+        const modalId = this.getAttribute('data-modal');
+
+        document.querySelectorAll('.modal').forEach(function(modal) {
+            modal.classList.remove('open');
+        });
+
+        if (modalId) {
+            document.getElementById(modalId).classList.add('open');
+        }
+        document.getElementById('mask').classList.add('open');
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+mask.addEventListener('click', () => {
+
+    document.querySelectorAll('.modal').forEach(modal => {
+        modal.classList.remove('open');
+    });
+    mask.classList.remove('open');
+    document.body.style.overflow = 'auto';
+});
+
+// スムーズスクロール
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    const href = this.getAttribute('href');
+    let target;
+    if (href === '#' || href === '') {
+      // ページトップへ
+      target = document.documentElement; // または document.body
+    } else {
+      target = document.querySelector(href);
+    }
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
